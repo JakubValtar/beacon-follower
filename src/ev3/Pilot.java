19ec042e1a0bfe635a562577595db5bf6036f067
Tesarova <evatesarov@gmail.com>
2016-05-05 11:44:47 +0200

    Pilot class implemented.

--- src/ev3/Pilot.java
+++ src/ev3/Pilot.java
@@ -1,29 +1,38 @@
 package ev3;
 
+import lejos.hardware.motor.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 public class Pilot {
 
-  public void forward() {
+  private DifferentialPilot pilot;
 
+  public Pilot() {
+    pilot = new DifferentialPilot(2.1f, 4.4f, Motor.C, Motor.B, true);
+  }
+
+  public void forward() {
+    pilot.forward();
   }
 
   public void steerLeft() {
-
-  }
+    pilot.steer(50);
+}
 
   public void steerRight() {
-
+    pilot.steer(-50);
   }
 
   public void stop() {
-
+    pilot.stop();
   }
 
   public void turnLeft() {
-
+    pilot.rotateLeft();
   }
 
   public void turnRight() {
-
+    pilot.rotateRight();
   }
 
 }
