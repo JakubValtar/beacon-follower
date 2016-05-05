64639bfb6e750db256d2b8e1d69e5878a97790d1
Jakub Valtar <jakub.valtar@gmail.com>
2016-05-05 14:57:19 +0200

    Use basic executor (fixes tasks not running)

--- src/ev3/Context.java
+++ src/ev3/Context.java
@@ -2,34 +2,20 @@ package ev3;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 
 public class Context {
 
   public final ExecutorService executor;
 
-  public Float beaconDistance;
-  public Float beaconDirection;
-  public Integer surfaceColor;
+  public float beaconDistance;
+  public float beaconDirection;
+  public int surfaceColor;
 
   public Pilot pilot;
   public SensorReader sensorReader;
 
   public Context() {
-    executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
-      @Override
-      public Thread newThread(Runnable r) {
-        Thread t = new Thread("EXECUTOR");
-        t.setPriority(Thread.MAX_PRIORITY);
-        t.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-          @Override
-          public void uncaughtException(Thread t, Throwable e) {
-            e.printStackTrace();
-          }
-        });
-        return t;
-      }
-    });
+    executor = Executors.newSingleThreadExecutor();
 
     pilot = new Pilot();
     sensorReader = new SensorReader();
